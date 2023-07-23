76eacce49a8c7d3e0c34c5f643751f4eec67b5f4
virupaksha <virupakshavegi123@gmail.com>
2023-07-23 14:41:53 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+import Description from "./components/Description";
+import { useEffect, useState } from "react";
+import getFormattedWeatherData from "./weatherService";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("Paris");
+  const [bg, setbg] = useState(hotBg);
+  // const [units, setunits] = useState("metric");
+  // const [city, setCity] = useState("Paris");
+  const [units, setunits] = useState("metric");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      // console.log(data);
+      setWeather(data);
+      //dynamic background image
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp <= threshold) setbg(coldBg);
+      else setbg(hotBg);
+    };
+    fetchWeatherData();
+  }, [units, city]);
+  // const fetchWeatherData = async () => {
+  //   const data = await getFormattedWeatherData(city, units);
+  // };
+  // fetchWeatherData();
+  // const handleUnitsClick = (e) => {
+  //   const button = e.currentTarget;
+  //   const currentunit = button.innerText.slice(1);
+  //   const isCelsius = currentunit === "C";
+  //   button.innerText = isCelsius ? "℉" : "℃";
+  //   setunits(isCelsius ? "metric" : "imperial");
+  // };
+  const handleUnitsClick = () => {
+    setunits((prevUnits) => (prevUnits === "metric" ? "imperial" : "metric"));
+  };
+  const enterkeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                onKeyDown={enterkeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City name.."
+              />
+              <button onClick={handleUnitsClick}>
+                {units === "metric" ? "℉" : "℃"}
+              </button>
+              {/* <button onClick={(e) => handleUnitsClick(e)}>℉</button> */}
+            </div>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>
+                  {weather.name},{weather.country}
+                </h3>
+                <img src={weather.iconURL} alt="weatherIcon"></img>
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+            {/* {bottom description} */}
+            <Description weather={weather} units={units} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/weatherService.js
@@ -0,0 +1,34 @@
+const makeIconURL = (iconID) =>
+  `https://openweathermap.org/img/wn/${iconID}@2x.png`;
+const API_KEY = "7ff21e5a5436ca2e7512269521e703e9";
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+  // console.log(data);
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+export default getFormattedWeatherData;
